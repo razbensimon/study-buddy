567bc640671933f885d4ce69054333f881784133
null-byt3 <null-byt3@users.noreply.github.com>
2021-01-04 19:33:07 +0200

    Refactored code. Added right-click functionality

--- background.js
+++ background.js
@@ -1,14 +1,15 @@
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-      console.log('The color is green.');
-    });
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-      chrome.declarativeContent.onPageChanged.addRules([{
-        conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: 'developer.chrome.com'},
-        })
-        ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-      }]);
-    });
-  });
+
+function getword(info,tab) {
+  if (info.menuItemId !== "STUDY_BUDDY") {
+    return;
+  }
+  chrome.tabs.create({url: "https://en.wikipedia.org/w/index.php?search=" + info.selectionText});
+}
+
+chrome.contextMenus.create({
+  title: "Search with StudyBuddy", 
+  contexts:["selection"], 
+  id: "STUDY_BUDDY"
+});
+
+chrome.contextMenus.onClicked.addListener(getword)
