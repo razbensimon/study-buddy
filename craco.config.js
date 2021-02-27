19765d5337b8ba92dd5cbd3fd101f88ec2f9d6ba
razbensimon <raz.bensimon@env0.com>
2021-02-28 00:51:15 +0200

    make background worker to work!

--- craco.config.js
+++ craco.config.js
@@ -11,11 +11,13 @@ module.exports = {
                 entry: {
                     main: [env === 'development' && require.resolve('react-dev-utils/webpackHotDevClient'), paths.appIndexJs].filter(Boolean),
                     content: './src/chrome/content.ts',
-                    //background: './src/chrome/background.ts',
+                    background: './src/chrome/background.ts'
                 },
                 output: {
                     ...webpackConfig.output,
-                    filename: 'static/js/[name].js',
+                    filename:(pathData) => {
+                        return pathData.chunk.name === 'background' ? '[name].js' : 'static/js/[name].js';
+                    }
                 },
                 optimization: {
                     ...webpackConfig.optimization,
