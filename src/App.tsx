c68b65b6c886ee5d530d59a7f9eeb1b64e7a8002
razbensimon <raz.bensimon@env0.com>
2021-02-27 23:03:50 +0200

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
+import {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {ChromeMessage, Sender} from "./types";
